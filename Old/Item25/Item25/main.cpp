590e9fc8a04b4d2b62045d352ddc20d1d78be8e6
songzp <coscomic.dota@gmail.com>
2016-02-01 21:34:08 +0800

    Item 25
    Item 25

---
+++ Old/Item25/Item25/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Item25
+//
+//  Created by SongZhiPing on 2/1/16.
+//  Copyright © 2016 SongZhiPing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
